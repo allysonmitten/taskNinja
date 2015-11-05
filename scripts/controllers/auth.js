70453f794ae9153feb26e893f26eae50497ac7f5
Allyson <allysonmichellewhite@gmail.com>
2015-11-04 23:57:39 -0400

    Task 3 complete
    Fixing bugs of previous version

--- scripts/controllers/auth.js
+++ scripts/controllers/auth.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('AuthController', function($scope, $location, Auth, toaster) {
+app.controller('AuthController', function($scope, $location, Auth, toaster) {
 
 	if(Auth.signedIn()) {
 		$location.path('/');
